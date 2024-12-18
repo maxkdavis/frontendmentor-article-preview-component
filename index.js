399edaf4a63c4fd47cef7bedd5ad0f6a59fda73b
maxkdavis <7098417+maxkdavis@users.noreply.github.com>
2024-12-18 14:28:34 -0500

    mobile - share button functionality

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const shareBtnEl = document.querySelector('.share-btn');
 const socialLinksContainerEl = document.querySelector('.social-links-container');
 
 shareBtnEl.addEventListener('click', function () {
-  console.log('clicked');
   socialLinksContainerEl.classList.toggle('share-open');
+  shareBtnEl.classList.toggle('btn-active');
 });
